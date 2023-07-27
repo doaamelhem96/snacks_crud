ed32971f945cd6c12d1612fdf3edfffe19b02e49
doaamelhem96 <drduaamelhem@gamil.com>
2023-07-27 21:04:53 +0300

    updating

--- snacks/admin.py
+++ snacks/admin.py
@@ -4,3 +4,4 @@ from .models import Snack
 
 admin.site.register(Snack)
 
+
--- snacks/tests.py
+++ snacks/tests.py
@@ -42,4 +42,67 @@ class Snacktest(TestCase):
         self.assertRedirects(response, reverse('details',args=[2]))
 
 
+    def test_update_view(self):
+        url = reverse('sna_updates',args=[self.snack.id])
+        data = {
+            'title' :'test_2',
+            'purchaser' : self.user.id,
+            'description' : 'anythings'
+        }
+        res = self.client.post(path=url,data=data,follow=True)
+        self.assertEqual(len(Snack.objects.all()),1)
+        self.assertTemplateUsed(res,'snack_list.html')
+        self.assertRedirects(res,reverse('snacklist'))
 
+    def test_delete_view(self):
+        url = reverse('sna_delete',args=[self.snack.id])
+        res = self.client.post(path=url,follow=True)
+        self.assertEqual(len(Snack.objects.all()),0)
+        self.assertTemplateUsed(res,'snack_list.html')
+        self.assertRedirects(res,reverse('snacklist'))
+
+    def test_fields_model(self):
+        self.assertEqual(self.snack.purchaser,self.user)
+        self.assertEqual(self.snack.title,'test')
+        self.assertEqual(self.snack.description,'anythings')
+class test_Home(TestCase):
+    def test_status_code(self):
+        url=reverse('home')
+        response=self.client.get (url)
+        self.assertEqual (response.status_code,200)
+        
+    def test_Template(self):
+        url=reverse('home')
+        response=self.client.get(url)
+        self.assertTemplateUsed(response,'home.html')
+class SnacksListViewsTest(TestCase):
+    def setUp(self):
+       
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword'
+        )
+
+       
+        self.snack = Snack.objects.create(
+            title='Test Snack',
+            purchaser=self.user,
+            description='A test snack'
+        )
+
+    def test_snacks_list_view(self):
+        url = reverse('snacklist')
+        response = self.client.get(url)
+
+        
+        self.assertEqual(response.status_code, 200)
+
+        
+        self.assertTemplateUsed(response, 'snack_list.html')
+
+       
+        self.assertIn('snack', response.context)
+
+       
+        self.assertEqual(response.context['snack'], self.snack)
